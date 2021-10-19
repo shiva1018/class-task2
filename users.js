1e1042f225241b3674484439a30dc4d13cd599d5
shiva1018 <91366925+shiva1018@users.noreply.github.com>
2021-10-19 14:48:02 +0530

    class task

---
+++ users.js
@@ -0,0 +1,71 @@
+
+document.body.innerHTML=`
+<div class="form-user">
+<input class="useradd" placeholder="enter name">
+<input class="userpic" placeholder="enter pic uRl">
+<button onclick="Adduser()">ADD</button>
+</div>
+<section class="user-container"></section>`;
+
+async function getAllusers() {
+    const data= await fetch("https://616ba6e516c3fa0017171788.mockapi.io/users",{method: "GET"})
+    const users=await data.json();
+
+const usercontainer=document.querySelector(".user-container");
+usercontainer.innerHTML=""
+users.forEach((user)=>{
+    usercontainer.innerHTML +=  `
+    <div class="user-list">
+    <img class="user-avatar" src=${user.avatar}>
+    <div>
+    <p class="user-name">${user.name}</p>
+    <button onclick="Edit-user-form(${user.id})">EDIT</button>
+    <button onclick="Deleteuser(${user.id})">DELETE</button>
+    <div class="user-form edit-${user.id}">
+    <input value=${user.name} class="edit-${user.id}-user-name" placeholder="Enter username"/>
+    <input value=${user.avater}class="edit-${user.id}-user-pic" placeholder="enter url"/>
+    <button onclick="save()">save</button>
+    </div>
+    </div>
+   </div>
+    ` ;
+
+});
+
+console.log(users)
+}
+getAllusers();
+
+async function Deleteuser(userid){
+    // console.log("dele....", userid)
+    const data= await fetch("https://616ba6e516c3fa0017171788.mockapi.io/users/" + userid,{method: "DELETE"})
+    getAllusers();
+}
+async function Adduser(){
+    console.log("add....")
+    const username=document.querySelector(".useradd").value;
+    const img=document.querySelector(".userpic").value;
+
+    const data= await fetch("https://616ba6e516c3fa0017171788.mockapi.io/users",
+    { 
+    method: "POST",
+    Headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({ name:username,avatar:img}),
+
+}
+);
+getAllusers();
+}
+async function Edituser(userid){
+    console.log("Edit....")
+    const edituser=document.querySelector(`.edit-${userid}`)
+    edituser.style.display=
+    edituser.style.display==="block" ? "none" : "block";
+}
+    
+
+
+    
+
+
+
